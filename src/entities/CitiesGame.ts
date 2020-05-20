2028d4bab6050e48109cd3bb26975fdce0b5b32c
Sergey <ryabovs@yahoo.com>
2020-05-20 04:37:01 +0200

    implements raw gameStart flow

--- src/entities/CitiesGame.ts
+++ src/entities/CitiesGame.ts
@@ -1,15 +1,65 @@
 import Game from './Game'
+import Database from './Database'
 
-export default class CitiesGame extends Game {
-    public start(): void {
+interface CitiesGameData {
+    id: number
+}
+
+enum GameStatus {
+    started = 'started',
+    notStarted = 'notStarted',
+}
+
+export default abstract class CitiesGame extends Game {
+    private constructor() {
+        super()
+    }
+
+    static async start(gameData: CitiesGameData): Promise<void> {
+        const record = await CitiesGame.getGame(gameData)
+
+        if (!record) {
+            Database.collection.insertOne(
+                {_id: gameData.id},
+                (error, result) => {
+                    // TODO: create a GameRecord Model / class
+                    if (error) console.error(error)
+                    console.log('inserted:', result)
+                }
+            )
+        }
+
+        if (record.GameStatus !== GameStatus.started) {
+            Database.collection.updateOne(
+                {
+                    _id: gameData.id,
+                },
+                {
+                    $set: {
+                        gameStatus: GameStatus.started,
+                    },
+                },
+                (error, result) => {
+                    if (error) console.error(error)
+                    console.log('updated:', result)
+                }
+            )
+        }
+
+        console.log('### record', record)
+    }
+
+    static async status(gameData: CitiesGameData): Promise<any> {
+        return await CitiesGame.getGame(gameData)
+    }
+
+    static end(): void {
         return
     }
 
-    public status(): void {
-        return
-    }
-
-    public end(): void {
-        return
+    private static async getGame(gameData: CitiesGameData): Promise<any> {
+        return await Database.collection.findOne({
+            _id: gameData.id,
+        })
     }
 }
