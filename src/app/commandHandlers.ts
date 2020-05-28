c486a0269dd9ea3c8f6bdd2a59c11cbbd1f8d775
Sergey <ryabovs@yahoo.com>
2020-05-28 03:37:37 +0200

    improves status command

--- src/app/commandHandlers.ts
+++ src/app/commandHandlers.ts
@@ -8,6 +8,11 @@ export const COMMANDS_REGEXP = new Map([
     ['any', new RegExp(/.+/)],
 ])
 
+const STATUS_REPLYES = new Map([
+    [GameStatus.started, 'Игра уже идет'],
+    [GameStatus.notStarted, 'Игра не началась'],
+])
+
 const handleOnStart = (bot): CommandHandler => {
     return async (msg): Promise<void> => {
         const { id } = msg.chat
@@ -22,8 +27,19 @@ const handleOnStatus = (bot): CommandHandler => {
         const { id } = msg.chat
         const game = new CitiesGame(id)
         const { status, history } = await game.status()
+        const cities = history
+            .reduce((cities, item) => {
+                cities.push(item[1])
+                return cities
+            }, [])
+            .join(', ')
 
-        bot.sendMessage(id, `Status: game ${status}. Game history: ${history}`)
+        bot.sendMessage(
+            id,
+            `Статус игры: ${STATUS_REPLYES.get(
+                status
+            )}. Названные города: ${cities}`
+        )
     }
 }
 
