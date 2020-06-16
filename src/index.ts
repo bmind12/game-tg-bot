826ffb34565ad28a6316ce6a4e0d4664a4880ef1
Sergey <ryabovs@yahoo.com>
2020-06-16 04:17:33 +0200

    removes not needed ngrok

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 require('dotenv').config()
-import ngrok from 'ngrok'
 import './globals'
 import App from './app'
 import TelegramBot from 'node-telegram-bot-api'
 // TODO: create module links
 
-const url = process.env.URL || '0'
+const url = process.env.URL
 const port = process.env.PORT || 8000
 const token = process.env.TELEGRAM_TOKEN
-const options = url === '0' ? { polling: true } : { webHook: { port: +port } }
+const options = url ? { webHook: { port: +port } } : { polling: true }
 
 if (token) {
     const bot = new TelegramBot(token, options)
 
     if (options.webHook) bot.setWebHook(`${url}/bot${token}`)
-
-    if (url === '0') ngrok.connect(+port)
 
     App.init(bot)
 }
