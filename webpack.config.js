5a47ba3cf5be01c4a591ce3d84122c6d971e964f
Sergey <ryabovs@yahoo.com>
2020-06-17 05:22:52 +0200

    improves build size significantly with wobpack and babel optimisation and cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
 const path = require('path')
 const { ContextReplacementPlugin } = require('webpack')
-// TODO: shrink build size
-module.exports = {
-    mode: 'development', // TODO: make dynamic
-    entry: ['@babel/polyfill', './src/index.ts'],
-    target: 'node',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                    {
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [new ContextReplacementPlugin(/require_optional/)],
-    resolve: {
-        extensions: ['.ts', 'tsx', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        chunkFilename: '[chunkhash].js',
-    },
+
+module.exports = (env) => {
+    const plugins = [new ContextReplacementPlugin(/require_optional/)]
+
+    return {
+        mode: env.mode,
+        entry: './src/index.ts',
+        target: 'node',
+        devtool: !env.production && 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                        },
+                        {
+                            loader: 'ts-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins,
+        resolve: {
+            extensions: ['.ts', 'tsx', '.js'],
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+        },
+    }
 }
